3bc8bc27ee72cccf650fc1d17c22cc74776ed3bf
simonW-open <simon_wang@outlook.com>
2020-03-29 22:31:41 +0800

    rebuild

--- components/esp-idf/http-parser/README.md
+++
@@ -1,3 +0,0 @@
-http-parser-espidf
-===============
-[ESP-IDF](https://github.com/espressif/esp-idf).
--- components/esp-idf/http-parser/component.mk
+++
@@ -1,7 +0,0 @@
-#
-# Component Makefile
-#
-COMPONENT_ADD_INCLUDEDIRS = .
-
-COMPONENT_SRCDIRS = http-parser
-COMPONENT_OBJS = http-parser/http_parser.o
